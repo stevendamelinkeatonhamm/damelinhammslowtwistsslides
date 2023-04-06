fc17b68832985c9f9961f52d15590330a0690ebd
DamelinHamm <125151628+DamelinHamm@users.noreply.github.com>
2023-04-06 05:04:52 -0400

    Add files via upload

--- SlowTwistR3Test.m
+++ SlowTwistR3Test.m
@@ -1,6 +1,6 @@
 clear all; close all; clc;
 %create some points
-x = -3:.05:3;
+x = -1:.05:1;
 y = [x; x; x];
 
 %w = -pi:.1:pi;
@@ -8,8 +8,8 @@ y = [x; x; x];
 
 a = 1/sqrt(3);
 b = 1/sqrt(3);
-c = 1/sqrt(6);
-d = 1/sqrt(6);
+c = 1/3;
+d = 1/3;
 
 Theta = [a^2+b^2-c^2-d^2 2*b*c-2*a*d 2*b*d+2*a*c; 2*b*c+2*a*d a^2-b^2+c^2-d^2  2*c*d-2*a*b; 2*b*d-2*a*c 2*c*d+2*a*b a^2-b^2-c^2+d^2];
 
@@ -17,9 +17,8 @@ Theta = [a^2+b^2-c^2-d^2 2*b*c-2*a*d 2*b*d+2*a*c; 2*b*c+2*a*d a^2-b^2+c^2-d^2  2
 
 f = @(t) exp(-.5*norm(t));
 %f = @(t) norm(t);
-%f = @(t) 1./(1+norm(t));
-%S = @(x) Theta'*[1 0 0; 0 cos(f(x)) sin(f(x)); 0 -sin(f(x)) cos(f(x))]*Theta*x;
-S = @(x) [1 0 0; 0 cos(f(x)) sin(f(x)); 0 -sin(f(x)) cos(f(x))]*x;
+f = @(t) 1./(1+norm(t));
+S = @(x) Theta'*[1 0 0; 0 cos(f(x)) sin(f(x)); 0 -sin(f(x)) cos(f(x))]*Theta*x;
 %S2 = @(x) [cos(f2(x)) sin(f2(x)) 0; -sin(f2(x)) cos(f2(x)) 0; 0 0 1]*x;
 for i=1:length(y)
     t(:,i) = S(y(:,i));
@@ -30,42 +29,9 @@ end
 for i=1:length(y)
     t3(:,i) = S(t2(:,i));
 end
-for i=1:length(y)
-    t4(:,i) = S(t3(:,i));
-end
-for i=1:length(y)
-    t5(:,i) = S(t4(:,i));
-end
 % figure;
 % plot3(y(1,:),y(2,:),y(3,:),'o',t(1,:),t(2,:),t(3,:),'o');
 % legend('Data','Twisted')
 figure;
-plot3(y(1,:),y(2,:),y(3,:),'o',t(1,:),t(2,:),t(3,:),'o',t2(1,:),t2(2,:),t2(3,:),'o',t3(1,:),t3(2,:),t3(3,:),'o',t4(1,:),t4(2,:),t4(3,:),'o',t5(1,:),t5(2,:),t5(3,:),'o');
-%legend('Data','1 Twist','2 Twist');
-view(-62.3, -19.6)
-%{
-figure;
-plot3(y(1,:),y(2,:),y(3,:),'o');
-view(-62.3, -19.6)
-saveas(gcf,'3DTwist_0.png');
-figure;
-plot3(t(1,:),t(2,:),t(3,:),'o');
-view(-62.3, -19.6)
-saveas(gcf,'3DTwist_1.png');
-figure;
-plot3(t2(1,:),t2(2,:),t2(3,:),'o');
-view(-62.3, -19.6)
-saveas(gcf,'3DTwist_2.png');
-figure;
-plot3(t3(1,:),t3(2,:),t3(3,:),'o');
-view(-62.3, -19.6)
-saveas(gcf,'3DTwist_3.png');
-figure;
-plot3(t4(1,:),t4(2,:),t4(3,:),'o');
-view(-62.3, -19.6)
-saveas(gcf,'3DTwist_4.png');
-figure;
-plot3(t5(1,:),t5(2,:),t5(3,:),'o');
-view(-62.3, -19.6)
-saveas(gcf,'3DTwist_5.png');
-%}
+plot3(y(1,:),y(2,:),y(3,:),'o',t(1,:),t(2,:),t(3,:),'o',t2(1,:),t2(2,:),t3(3,:),'o');
+legend('Data','1 Twist','2 Twist');
--- SlowTwistTest.m
+++ SlowTwistTest.m
@@ -3,15 +3,15 @@ clear all; close all; clc;
 x = -5:.05:5;
 y = [x;x];
 
-w = -pi:.1:pi;
-y = [cos(w)+1; sin(w)+1];
+%w = -pi:.1:pi;
+%y = [cos(w)+.1; sin(w)+.1];
 
-f = @(t) exp(-0.1*norm(t));
+f = @(t) exp(-1*norm(t));
 %f = @(t) norm(t);
 %f = @(t) 1./(1+norm(t));
-%Theta = [cos(pi/4) -sin(pi/4); sin(pi/4) cos(pi/4)];
+Theta = [cos(pi/4) -sin(pi/4); sin(pi/4) cos(pi/4)];
 S = @(x) [cos(f(x)) sin(f(x)); -sin(f(x)) cos(f(x))]*x;
-%S = @(x) [cos(f(Theta*x)) sin(f(Theta*x)); -sin(f(Theta*x)) cos(f(Theta*x))]*x;
+S = @(x) [cos(f(Theta*x)) sin(f(Theta*x)); -sin(f(Theta*x)) cos(f(Theta*x))]*x;
 for i=1:length(y)
     t(:,i) = S(y(:,i));
 end
@@ -30,100 +30,24 @@ end
 for i=1:length(y)
     t6(:,i) = S(t5(:,i));
 end
-for i=1:length(y)
-    t7(:,i) = S(t6(:,i));
-end
-for i=1:length(y)
-    t8(:,i) = S(t7(:,i));
-end
-for i=1:length(y)
-    t9(:,i) = S(t8(:,i));
-end
-for i=1:length(y)
-    t10(:,i) = S(t9(:,i));
-end
-for i=1:length(y)
-    t11(:,i) = S(t10(:,i));
-end
-for i=1:length(y)
-    t12(:,i) = S(t11(:,i));
-end
-for i=1:length(y)
-    t13(:,i) = S(t12(:,i));
-end
-for i=1:length(y)
-    t14(:,i) = S(t13(:,i));
-end
-for i=1:length(y)
-    t15(:,i) = S(t14(:,i));
-end
 % figure;
 % plot(y(1,:),y(2,:),'o',t(1,:),t(2,:),'o');
 % legend('Data','Twisted')
 figure;
-%fig = plot(y(1,:),y(2,:),'o',t(1,:),t(2,:),'o',t2(1,:),t2(2,:),'o',t3(1,:),t3(2,:),'o');
-axis([-2 2 -2 2]); 
-plot(y(1,:),y(2,:),'o');
- saveas(gcf,'CircleTwist_0.png');
-figure;
-plot(t(1,:),t(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_1.png');
-figure;
-plot(t2(1,:),t2(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_2.png');
-figure;
-plot(t3(1,:),t3(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_3.png');
-figure;
-plot(t4(1,:),t4(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_4.png');
-figure;
-plot(t5(1,:),t5(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_5.png');
-figure;
-plot(t6(1,:),t6(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_6.png');
-figure;
-plot(t7(1,:),t7(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_7.png');
-figure;
-plot(t8(1,:),t8(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_8.png');
-figure;
-plot(t9(1,:),t9(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_9.png');
-figure;
-plot(t10(1,:),t10(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_10.png');
-figure;
-plot(t11(1,:),t11(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_11.png');
-figure;
-plot(t12(1,:),t12(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_12.png');
-figure;
-plot(t13(1,:),t13(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_13.png');
-figure;
-plot(t14(1,:),t14(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_14.png');
-figure;
-plot(t15(1,:),t15(2,:),'o');
-axis([-2 2 -2 2]); 
-saveas(gcf,'CircleTwist_15.png');
+plot(y(1,:),y(2,:),'o',t(1,:),t(2,:),'o',t2(1,:),t2(2,:),'o',t3(1,:),t3(2,:),'o');
+% plot(y(1,:),y(2,:),'o');
+% figure;
+% plot(t(1,:),t(2,:),'o');
+% figure;
+% plot(t2(1,:),t2(2,:),'o');
+% figure;
+% plot(t3(1,:),t3(2,:),'o');
+% figure;
+% plot(t4(1,:),t4(2,:),'o');
+% figure;
+% plot(t5(1,:),t5(2,:),'o');
+% figure;
+% plot(t6(1,:),t6(2,:),'o');
+% figure;
 % plot(y(1,:),y(2,:),'o',t(1,:),t(2,:),'o',t2(1,:),t2(2,:),'o',t3(1,:),t3(2,:),'o',t4(1,:),t4(2,:),'o',t5(1,:),t5(2,:),'o',t6(1,:),t6(2,:),'o');
 %legend('Data','1 Twist','2 Twist','3 Twist');
